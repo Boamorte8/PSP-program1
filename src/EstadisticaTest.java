672a3a0e016f97f74fc5098eddca0e9ca1d0505b
James Garzón <jamesgarzon92@gmail.com>
2016-08-18 17:35:39 -0500

    Se actualizan las funciones para utilizar lista ligadas

--- src/EstadisticaTest.java
+++ src/EstadisticaTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class EstadisticaTest {
@@ -11,37 +13,37 @@ public class EstadisticaTest {
 	}
 	
 	@Test
-	public void getMediaTestInts(){
+	public void getMeanTest(){
 		Statistic statistic = new Statistic();
-		int[] numbers = {160,591,114,229,230,270,128,1657,624,1503};
-		float mean = statistic.getMean(numbers);
+		LinkedList<Float> numberList = new LinkedList<Float>();
+		String numbers = "160,591,114,229,230,270,128,1657,624,1503";
+		numberList = statistic.getListFromString(numbers);
+		float mean = statistic.getMean(numberList);
 		assertEquals("Is not the media expected. ", 550.6, mean, 0.1);
 	}
 	
-	@Test
-	public void getMediaTestFloats(){
-		Statistic statistic = new Statistic();
-		float[] numbers = {15.0f,69.9f,6.5f,22.4f,28.4f,65.9f,19.4f,198.7f,38.8f,138.2f};
-		float mean = statistic.getMean(numbers);
-		assertEquals("Is not the media expected. ", 60.32, mean, 0.1);
-	}
-	
 
-	@Test
+@Test
 	public void getStandarDeviation(){
 		Statistic statistic = new Statistic();
-		int[] numbers = {160,591,114,229,230,270,128,1657,624,1503};
-		double standarDeviation  = statistic.getStandarDeviation(numbers);
+		LinkedList<Float> numberList = new LinkedList<Float>();
+		String numbers = "160,591,114,229,230,270,128,1657,624,1503";
+		numberList = statistic.getListFromString(numbers);
+		float standarDeviation = statistic.getStandarDeviation(numberList);
 		assertEquals("Its not the expected standar deviation", 542.67, standarDeviation, 0.01);
 	}
 	
-	@Test
+@Test
 	public void getVariance(){
 		Statistic statistic = new Statistic();
-		int[] numbers = {160,591,114,229,230,270,128,1657,624,1503};
-		float variance  = statistic.getVariance(numbers);
+		LinkedList<Float> numberList = new LinkedList<Float>();
+		String numbers = "160,591,114,229,230,270,128,1657,624,1503";
+		numberList = statistic.getListFromString(numbers);
+		float variance  = statistic.getVariance(numberList);
 		assertEquals("Its not the expected variance", 294493.24, variance, 0.1);
 	}
+	
+
 	
 
 }
--- src/Statistic.java
+++ src/Statistic.java
@@ -1,46 +1,99 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Statistic {
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	public static void main (String arg[]){
 
-	public float getMean(float[] numbers) {
+	}
+
+	public float getMean( LinkedList<Float> numberList) {
+		
 		float sum = (float) 0.0;
 		float mean = (float) 0.0;
-		for (int i = 0; i < numbers.length; i++) {
-			sum += numbers[i];
+		for (Float number : numberList) {
+			sum += number;
 		}
-		mean = sum/numbers.length;
+		mean = sum/numberList.size();
 		return mean;
 	}
 	
-	public float getMean(int[] numbers) {
-		float[] floatNumbers = castIntArraytoFloatArray(numbers);
-		float mean = getMean(floatNumbers);
-		return mean;
-	}
+//	public float getMean(int[] numbers) {
+//		float[] floatNumbers = castIntArraytoFloatArray(numbers);
+//		float mean = getMean(floatNumbers);
+//		return mean;
+//	}
 
-	public double getStandarDeviation(int[] numbers) {
+	public float getStandarDeviation(LinkedList<Float> numberList) {
 		float standarDeviation = 0;
-		float variance = getVariance(numbers);
+		float variance = getVariance(numberList);
 		standarDeviation = (float) Math.sqrt(variance);
 		return standarDeviation;
 	}
 
-	public float getVariance(int[] numbers) {
-		float mean = getMean(numbers);
+	public float getVariance(LinkedList<Float> numberList) {
+		float mean = getMean(numberList);
 		float variance = 0;
-		for (int i = 0; i < numbers.length; i++) {
-			variance += Math.pow(numbers[i]-mean, 2);
+		for (Float number : numberList) {
+			variance += Math.pow(number-mean, 2);
 		}
-		variance = variance/numbers.length;
+		
+//		for (int i = 0; i < numberList.length; i++) {
+//			variance += Math.pow(numberList[i]-mean, 2);
+//		}
+		variance = variance/numberList.size();
 		return variance;
 	}
 	
-	public float[] castIntArraytoFloatArray(int[] array){
-		float[] floatNumbers = new float[array.length];
-		for (int i = 0; i < array.length; i++) {
-			floatNumbers[i] = (float) Float.valueOf(array[i]);
-		}
-		return floatNumbers;
+//	public float[] castIntArraytoFloatArray(int[] array){
+//		float[] floatNumbers = new float[array.length];
+//		for (int i = 0; i < array.length; i++) {
+//			floatNumbers[i] = (float) Float.valueOf(array[i]);
+//		}
+//		return floatNumbers;
+//	}
+	
+//	public void createLinkedList(){
+//		LinkedList<Float> list = new LinkedList<Float>();
+//		list.add(71.1f);
+//		list.add(78.1f);
+//		Iterator<Float> it = list.iterator();
+//		for (Float float1 : list) {
+//			System.out.println(float1);	
+//		}
+//		
+//	}
+	
+	public void getNumbers(){
+	    System.out.print("Ingrese los numeros separados por ',' (Ejm. 4,5,8,7) : \n");
+	    String input = scanner.nextLine();
+	    System.out.println("input : " + input);
+	    System.out.println("-----------\n");
+	    scanner.close();
+	    getListFromString(input);
 	}
+	
+	public LinkedList<Float> getListFromString(String stringList){
+		LinkedList<Float> list = new LinkedList<Float>();
+		String[] floatString = stringList.split(",");
+		for (int i = 0; i < floatString.length; i++) {
+			list.add(Float.parseFloat(floatString[i]));
+		}
+		return list;
+	}
+	
+	public void printList(LinkedList<Float> list){
+		for (Float float1 : list) {
+			System.out.print(float1+ " - ");	
+		}
+	}
+	
+	
+	
+	
 	
 
 }
